b3a15c9c765c7116049012bbb4579b2026cc72b7
Ryan Manchester <ryanm17@gmail.com>
2017-10-30 02:19:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, data|
     data.each do |holiday, supply|
       if supply.include?("BBQ")
-        holiday
+        holiday.to_s
         #binding.pry
       end
     end
