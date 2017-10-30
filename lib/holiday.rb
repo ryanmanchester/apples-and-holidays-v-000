25c00d6357c11da915e5181102dff3c33ab0687b
Ryan Manchester <ryanm17@gmail.com>
2017-10-30 00:29:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season}:".capitalize!
    data.each do |holiday, supply|
-      puts "  #{holiday}: #{supply}".to_s.capitalize!
+      puts "  #{holiday}:".to_s.capitalize! "#{supply}"
 
     end
   end
